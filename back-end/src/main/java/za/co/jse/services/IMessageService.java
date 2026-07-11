2309f4c2c3b12acb9102b4e5afaf002f5b598548
Buhle M <mthembbu@gmail.com>
2026-07-11 17:43:42 +0200

    return message for add message

--- back-end/src/main/java/za/co/jse/services/IMessageService.java
+++ back-end/src/main/java/za/co/jse/services/IMessageService.java
@@ -1,10 +1,11 @@
 package za.co.jse.services;
 
+import za.co.jse.entities.Message;
 import za.co.jse.entities.dtos.MessageDto;
 import za.co.jse.entities.dtos.MessageRespDto;
 
 public interface IMessageService {
     MessageRespDto findByUsername(String username);
 
-    void addMessage(MessageDto messageDto);
+    Message addMessage(MessageDto messageDto);
 }
