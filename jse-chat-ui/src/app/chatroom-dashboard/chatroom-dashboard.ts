faf7f3c4dcc024cebe4be543358a641b96078676
Buhle M <mthembbu@gmail.com>
2026-07-12 17:33:50 +0200

    second commit for angular ui

---
+++ jse-chat-ui/src/app/chatroom-dashboard/chatroom-dashboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chatroom-dashboard',
+  imports: [],
+  templateUrl: './chatroom-dashboard.html',
+  styleUrl: './chatroom-dashboard.scss',
+})
+export class ChatroomDashboard {
+
+}
